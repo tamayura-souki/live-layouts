53bda43896bc0ddfc4df1357abd6712aedbd9ea2
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2023-12-02 20:13:53 +0900

    refactor: add comments

--- onecomment-template/like-cui/script.js
+++ onecomment-template/like-cui/script.js
@@ -1,13 +1,27 @@
+/** ワンコメ側の設定 */
 const JSON_PATH = '../../comment.json'
-const LIMIT = 30
+/** ワンコメ側の設定 */
+const COMMENT_LIMIT = 30
+
+/** コメントの 1文字が表示されてから次の文字が表示されるまでの時間 単位ms */
 const TYPING_INTERVAL = 100
+/** コメントが表示されてから最初の文字が表示されるまでの時間 単位ms */
 const TYPING_DELAY = 200
 
+/**
+ * @param {String} html 1回のタイプで表示したい html
+ * @param {number} index 何文字目にタイプするか (ディレイの時間調整に使用)
+ * @returns {String}
+ */
 const htmlToTypeChar = (html, index) => {
   const delayMiliSecond = index * TYPING_INTERVAL+TYPING_DELAY
   return `<div class='char' style='animation-delay:${delayMiliSecond}ms'>${html}</div>`
 }
 
+/**
+ * @param {String} comment コメントの文字列 (絵文字が img タグで混入している)
+ * @returns タイピングアニメーションを付与した comment
+ */
 const attachTypingAnim = (comment) => {
   const imgReg = /<img [^<>]+>/g
   const imgTags = [...comment.matchAll(imgReg)]
@@ -62,7 +76,7 @@ const app = Vue.createApp({
     commentIndex = 0
     OneSDK.setup({
       jsonPath: JSON_PATH,
-      commentLimit: LIMIT
+      commentLimit: COMMENT_LIMIT
     })
     OneSDK.subscribe({
       action: 'comments',
